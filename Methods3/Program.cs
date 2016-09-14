179e298a270ddcaeff6838d7e34a8a5bf0ebf3a6
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-14 13:51:58 -0400

    Initial Commit.

---
+++ Methods3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class Program
+    {
+        static void Main(string[] args)            
+        {
+            Console.WriteLine("Select a number");
+            string number = Console.ReadLine();
+            Console.WriteLine(ReverseNumbers(number));
+        }
+        static string ReverseNumbers(string number)
+        {
+            string answer = "";
+            for (int i = (number.Length - 1); i >= 0; i--)
+            {
+                answer += number[i];
+            }
+            return answer;
+        }
+    }
+}
